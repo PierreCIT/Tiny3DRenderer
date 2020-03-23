a92652199fa8c9c26c0f2c9182d8cbbef697cff6
Noeim <noeim.jeux@gmail.com>
2020-03-23 17:30:27 +0100

    add: draw line in an optimized way

---
+++ main.h
@@ -0,0 +1,15 @@
+//
+// Created by noeim on 23/03/2020.
+//
+
+#ifndef TINY3DRENDERER_MAIN_H
+#define TINY3DRENDERER_MAIN_H
+
+#include <iostream>
+#include "tgaimage.h"
+
+using namespace std;
+
+bool line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
+
+#endif //TINY3DRENDERER_MAIN_H
